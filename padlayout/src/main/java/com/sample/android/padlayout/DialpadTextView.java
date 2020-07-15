54add50b0699a84a932047275133abe2800211f8
Ali Rezaei <ali.rezaei09@gmail.com>
2020-07-15 20:02:24 +0430

    Android Open Source Project Licence

--- padlayout/src/main/java/com/sample/android/padlayout/DialpadTextView.java
+++ padlayout/src/main/java/com/sample/android/padlayout/DialpadTextView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 package com.sample.android.padlayout;
 
 import android.content.Context;
@@ -7,6 +22,14 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+/**
+ * This is a custom text view intended only for rendering the numerals (and star and pound) on the
+ * dialpad. TextView has built in top/bottom padding to help account for ascenders/descenders.
+ *
+ * Since vertical space is at a premium on the dialpad, particularly if the font size is scaled to
+ * a larger default, for the dialpad we use this class to more precisely render characters according
+ * to the precise amount of space they need.
+ */
 public class DialpadTextView extends TextView {
     private Rect mTextBounds = new Rect();
     private String mTextStr;
